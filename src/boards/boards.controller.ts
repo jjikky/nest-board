98d10dd6e7ccc0741f875dd182a9846541428d0b
jjikky <kok5417@skuniv.ac.kr>
2024-01-08 20:13:43 +0900

    ✨ get, delete, update 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
@@ -14,5 +14,20 @@ export class BoardsController {
 	@Post()
 	createBoard(@Body() createBoardDto: CreateBoardDto): Board {
 		return this.boardsService.createBoard(createBoardDto);
+	}
+
+	@Get('/:id')
+	getBoardById(@Param('id') id: string): Board {
+		return this.boardsService.getBoardById(id);
+	}
+
+	@Delete('/:id')
+	deleteBoard(@Param('id') id: string): void {
+		return this.boardsService.deleteBoard(id);
+	}
+
+	@Patch('/:id/status')
+	updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+		return this.boardsService.updateBoardStatus(id, status);
 	}
 }
