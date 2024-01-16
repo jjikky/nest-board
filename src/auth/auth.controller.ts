e42f2a226ef12aa3088e1279ea94cb39f2d80fe1
jjikky <kok5417@skuniv.ac.kr>
2024-01-16 16:50:47 +0900

    ✨ sign in - jwt 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
 		return this.authService.signUp(authCredentialsDto);
 	}
 	@Post('/signin')
-	signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<string> {
+	signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
 		return this.authService.signIn(authCredentialsDto);
 	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([UserRepository])],
+	imports: [
+		PassportModule.register({ defaultStrategy: 'jwt' }),
+		JwtModule.register({
+			secret: 'Secret1234',
+			signOptions: {
+				expiresIn: 60 * 60,
+			},
+		}),
+		TypeOrmModule.forFeature([UserRepository]),
+	],
 	controllers: [AuthController],
 	providers: [AuthService],
 })
